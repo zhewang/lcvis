90b278e1eedd432f91eee808f95510c8da746fd0
Zhe Wang <zwang.contact@gmail.com>
2015-07-13 16:23:27 -0700

    beautify code

--- main.js
+++ main.js
@@ -6,23 +6,24 @@ var app = express();
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.get('/', function (req, res) {
-    res.sendfile('index.html');
+app.get('/', function(req, res) {
+  res.sendfile('index.html');
 });
 
-app.post('/plotusers', function (req, res) {
-    console.log(req.body);
+app.post('/plotusers', function(req, res) {
+  console.log(req.body);
 
-    var p = process.spawn(
-        'python3',
-        ['/Users/zhe/Projects/lcvis/python_scripts/process_object.py']
-    );
+  var p = process.spawn(
+    'python3', ['/Users/zhe/Projects/lcvis/python_scripts/process_object.py']
+  );
 
-    p.stdout.on('data', function(data){
-        res.send(data);
-    });
+  p.stdout.on('data', function(data) {
+    res.send(data);
+  });
 });
 
 app.listen(8000);
